a64fad11b0c06de7650a2561ce6c1ba6d806e3dc
Anebi Emmanuel <anebiemmanuel@gmail.com>
2021-08-27 19:33:23 +0100

    Initial Stable Dev State

---
+++ account/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+class Student(models.Model):
+    QUALIFICATIONS = [
+        ('SECONDARY', 'Secondary'),
+        ('BACHELORS', 'Bachelors'),
+        ('MASTERS', 'Masters'),
+        ('P.HD', 'P.hD'),
+    ]
+
+    # create a relationship with User
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='student_profile', help_text=_("Your Profile Picture"))
+    university = models.CharField(max_length=256, help_text=_("The University You Attended."))
+    highest_qualification = models.CharField(max_length=12, choices=QUALIFICATIONS,
+                                             help_text= _("Your Highest Qualification Obtained"),
+                                             default="Bachelors")
+
+    def __str__(self):
+        return self.user.get_full_name()
